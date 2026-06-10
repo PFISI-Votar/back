6c7e8aba526c241c32e636b8bab0dd382fb954c8
Bruno Lucarelli <brunolucarelli5@gmail.com>
2026-06-09 21:21:18 -0300

    build(db): TypeORM and migrations config initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getDatabaseConfig } from './config/database.config';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getDatabaseConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/env.validation.ts
@@ -0,0 +1,11 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  DEVELOPMENT: Joi.boolean().truthy('true').falsy('false').default(false),
+  PORT: Joi.number().port().default(3000),
+  DB_HOST: Joi.string().hostname().required(),
+  DB_PORT: Joi.number().port().default(5432),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+});
